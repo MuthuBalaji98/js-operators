9d4c4a8ba6228e9a4e115b3e7eb922be7803ee39
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-10 19:39:19 +0530

    operators

---
+++ main.js
@@ -0,0 +1,51 @@
+// Assignment operator
+let a=10;
+a++;
+console.log(a);
+// console.log(++a);
+
+let b=10;
+let c=20;
+console.log(b+=c);
+console.log(b-=c);
+console.log(b*=c);
+console.log(b/=c);
+console.log(b%=c);
+
+// multiplication operator
+
+let x=5;
+let y=10;
+let mul=x*y;
+console.log(mul);
+
+var s = 10;
+s+= 5;
+console.log(s);
+
+let h=5;
+let i=4;
+let w=h**i;
+console.log(w);
+
+let text1="Muthu";
+let text2="Balaji";
+let text3=text1+" "+text2;
+console.log(text3);
+
+let text4 = "Have a very ";
+text4 += "nice day";
+console.log(text4);
+
+
+let r = 5 + 5;
+console.log(r);
+let v = "5" + 3;
+console.log(v);
+let z = "Muthu" + 5;
+console.log(z);
+
+let t=5;
+let q=10;
+console.log(t>q?t:q);
+
